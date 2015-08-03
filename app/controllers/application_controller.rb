3d48c8b3a64c20da36d3aeda73de834dc064b56d
Yukai Huang <yukaihuang1993@hotmail.com>
2015-08-03 23:17:39 +0800

    use colorgysso

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
+  include ColorgyDeviseSSOManager
+  include FlashMessageReporter
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  # include ColorgyDeviseSSOManager
-  # include FlashMessageReporter
+
+  if ENV['DISABLE_SSO'] == 'true'
+    before_filter :sso_off!
+  end
 end
