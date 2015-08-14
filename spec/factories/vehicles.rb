1034fa5b1562f8fe1cf4a5dd7d50d04acd91e1d2
Neson <neson@dex.tw>
2015-08-14 18:21:58 +0800

    add seat_info to vehicle factory

--- spec/factories/vehicles.rb
+++ spec/factories/vehicles.rb
@@ -4,6 +4,22 @@ FactoryGirl.define do
     capacity { [35, 50].sample }
     registration_number { Faker::Address.postcode }
     description { Faker::Lorem.paragraph }
-    # seat_info { Faker::Lorem.paragraph }
+    seat_info_string do
+      '{
+        "seating": [
+          [ "1",  "2", null, null],
+          [ "5",  "6", null,  "3"],
+          [ "8",  "9", null,  "7"],
+          [ "11", "12", null, "10"],
+          ["15", "16", null, "13"],
+          ["18", "19", null, "17"],
+          ["20", "21", null, "25"],
+          ["22", "23", null, "28"],
+          ["26", "27", null, "29"],
+          ["30", "31", null, "32"],
+          ["33", "35", "36", "37"]
+        ]
+      }'
+    end
   end
 end
