b8902176abd0bbed76438447650e19490a1bb1ee
Yukai Huang <yukaihuang1993@hotmail.com>
2015-08-28 10:10:57 +0800

    Update front-end / bill admin

--- app/admin/bill.rb
+++ app/admin/bill.rb
@@ -14,5 +14,22 @@ ActiveAdmin.register Bill do
   #   permitted
   # end
 
-
+  index do
+    selectable_column
+    column(:id)
+    column(:user_id) { |bill| a bill.user.name, href: admin_user_path(bill.user) }
+    column(:price)
+    column(:amount)
+    column(:invoice_type)
+    column(:type)
+    column(:state) { |bill| status_tag bill.state }
+    column(:payment_code)
+    column(:virtual_account)
+    column(:paid_at)
+    column(:used_credits)
+    column(:deadline)
+    column(:created_at)
+    column(:updated_at)
+    actions
+  end
 end
