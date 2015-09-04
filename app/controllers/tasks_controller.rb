35f11e22c0c10e74e70f55c4a519b47c19f3f297
Yukai Huang <yukaihuang1993@hotmail.com>
2015-09-04 17:14:30 +0800

    update bill export / set deadline timezone

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,19 +18,43 @@ class TasksController < ApplicationController
   def bill_export
     filename = Rails.root.join('tmp', "bus_bills_#{Time.now.strftime('%Y-%m-%d-%H_%M_%S')}.csv")
     CSV.open(filename, 'w') do |csv|
-      csv << %w(訂票人 狀態 路線 時間 張數 取票人 取票人email 取票人手機 取票人身份證)
+      csv << %w(訂票人 狀態 學校 FB 路線 時間 張數 取票人 取票人email 取票人手機 取票人身份證)
 
       Bill.paid.each do |bill|
         order = bill.orders.first
         next if order.schedule.nil?
         dept_time = order.schedule.departure_time.strftime('%Y/%m/%d %H:%M')
-        csv << [bill.user.name, bill.state, order.schedule.route.short_name, dept_time, bill.orders_count, order.receiver_name, order.receiver_email, order.receiver_phone, order.receiver_identity_number]
+        csv << [
+          bill.user.name,
+          bill.state,
+          bill.user.organization_code,
+          bill.user.fbid && "https://facebook.com/#{bill.user.fbid}",
+          order.schedule.route.short_name,
+          dept_time,
+          bill.orders_count,
+          order.receiver_name,
+          order.receiver_email,
+          order.receiver_phone,
+          order.receiver_identity_number
+        ]
       end
 
       Bill.payment_pending.each do |bill|
         order = bill.orders.first
         dept_time = order.schedule.departure_time.strftime('%Y/%m/%d %H:%M')
-        csv << [bill.user.name, bill.state, order.schedule.route.short_name, dept_time, bill.orders_count, order.receiver_name, order.receiver_email, order.receiver_phone, order.receiver_identity_number]
+        csv << [
+          bill.user.name,
+          bill.state,
+          bill.user.organization_code,
+          bill.user.fbid && "https://facebook.com/#{bill.user.fbid}",
+          order.schedule.route.short_name,
+          dept_time,
+          bill.orders_count,
+          order.receiver_name,
+          order.receiver_email,
+          order.receiver_phone,
+          order.receiver_identity_number
+        ]
       end
     end
 
